70b794359b128bfdef19321b72d0d7b1d391b9c1
Chris Oosthuizen <01474603@wf.uct.ac.za>
2024-11-14 13:47:57 +0000

    First draft corrections

--- scripts/4.1 2024_wiggle analysis and figures.R
+++ scripts/4.1 2024_wiggle analysis and figures.R
@@ -999,7 +999,7 @@ fig5_Simeone = Density_map_wiggleN_Simeone %>%
         axis.text.x=element_blank()) +
   theme(#legend.text=element_text(size = 8),
     #           legend.title=element_text(size = 10),
-    legend.key.size=unit(0.8, "lines")) + coord_flip()
+    legend.key.size=unit(0.8, "lines")) #+ coord_flip()
 
 fig5_Simeone
 
@@ -1101,10 +1101,44 @@ figure5 =  fig5_Simeone + fig5_Takahashi2 +
 figure5
 
 # Save as PNG
-ggsave("./figures/Figure4_points_density.png", figure5, width = 8, height = 6, dpi = 1500)
+#ggsave("./figures/Figure5_points_density.png", figure5, width = 8, height = 6, dpi = 1500)
 
 # Save as PNG
-ggsave("./figures/Figure6_bottist.png", fig5_bottdist, width = 4.2, height = 3.35, dpi = 1500)
+#ggsave("./figures/Figure5_bottist.png", fig5_bottdist, width = 4.2, height = 3.35, dpi = 1500)
+
+
+#----------------------------------
+# Figure 6 - examples
+#----------------------------------
+
+figure6_example_ids = Density_map_wiggleN_Halsey_2m %>% 
+  filter(id == "2022_01_13_AC2105_HPM09" | 
+           id == "2022_01_16_AC2105_DI06" | 
+           id == "2022_01_22_AC2110_DI06" | 
+           id == "2022_01_10_AC2003_DI09") %>%
+  ggplot(aes(x = wiggleN_Halsey_2m, y =video_pce_IN, color = Density)) +
+  geom_jitter(alpha = 0.4, size = 1.9, height = 0, width = 0) +
+  scale_color_viridis_c(option = 'inferno', end = 0.9, begin = 0.1)+
+  gg_theme() +
+  geom_abline(intercept = 0, slope = 1, linetype = "dashed", color = "black")+
+  ylab("Video prey captures") +
+  xlab("No. wiggles") +
+  scale_y_continuous(limits = c(0,80), breaks = seq(0,100, by = 25))+
+  scale_x_continuous(limits = c(-0.1, 6.1)) +
+  theme(legend.position = "none")+ 
+  theme(strip.background = element_blank(),
+        strip.text.x = element_blank()) +  
+  facet_wrap(~id, scales = "fixed", ncol = 2) + 
+  theme(
+    legend.position = "inside", legend.position.inside = c(0.99, 0.99),  # Adjust legend position
+    legend.justification = c(1, 1),  # Justify legend to bottom-right
+    legend.box.just = "right") + 
+  theme(legend.key.size=unit(0.6, "lines")) 
+
+figure6_example_ids
+
+# Save as PNG
+ggsave("./figures/Figure6.png", figure6_example_ids, width = 4, height = 4, dpi = 1000)
 
 
 #----------------------------------
@@ -1399,7 +1433,7 @@ PCE_cumulative = wrap_plots(plots, ncol = 2, nrow = 5)
 PCE_cumulative 
 
 # Save as PNG
-ggsave("./supplement/Fig_videoPCE_cumulative_wiggles.png", 
+ggsave("./figures/Figure5.png", 
        PCE_cumulative, width = 8, height = 10, dpi = 1200)
 
 
@@ -2307,7 +2341,7 @@ cpue_tanglegram
 tau_list
 
 # Save as PNG
-ggsave("./figures/figure4.png", cpue_tanglegram, width = 10, height = 6.5, dpi = 1200)
+ggsave("./figures/Figure4.png", cpue_tanglegram, width = 10, height = 6.5, dpi = 1200)
 
 
 #--------tanglegrams end------------------------------------------------------------------
